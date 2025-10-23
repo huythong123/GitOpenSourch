df8788280617b861431f558134e6499110f7c1c5
mkhanh2202 <khanhtruong461@gmail.com>
2025-10-23 15:29:34 +0700

    cau11 mkhanh

---
+++ cau11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void inTamGiac(int n) {
+	int i,j ;
+    for ( i = 1; i <= n; i++) {
+        for ( j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
