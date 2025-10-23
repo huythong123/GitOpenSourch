8b29e4bde50fdf6193ab3c920c5e3d79eab8b97d
huythong123 <huythong983@gmail.com>
2025-10-23 15:52:52 +0700

    add cau6 ht

---
+++ cau6.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void inBangCuuChuong(int n) {
+    printf("Bang cuu chuong %d:\n", n);
+    for (int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+}
